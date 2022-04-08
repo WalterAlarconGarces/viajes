968be202a9a6f5d647808504148f01c07bf4f6a2
Walter Alarcon Garces <walarcon@gmail.com>
2022-04-08 02:57:57 -0400

    Adicion de comentarios en JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
+//Elementos para el funcionamiento de tooltips
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
+//Elemento de jquery
 $("#enviar").click(function () { 
         
     alert("Contacto exitoso")
